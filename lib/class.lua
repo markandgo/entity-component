87cb0fd02848ce7764d0cac8c0d73c329e9f4d0f
Minh Ngo <mkngo87@gmail.com>
2013-06-23 01:04:58 -0400

    Class changes and addition: Class:clone Class:new

--- lib/class.lua
+++ lib/class.lua
@@ -3,30 +3,31 @@
 Class          = {__type = 'Object'}
 Class.__index  = Class
 
-function Class.type(obj)
-	return obj.__type
+function Class:new(...)
+	self.__index = self
+	self.__call  = Class.new
+	local obj    = setmetatable({},self)
+	if self.init then self.init(obj,...) end
+	return obj
 end
 
-function Class.typeOf(obj,name)
-	while obj do
-		if obj.__type == name then return true end
-		local meta = getmetatable(obj)
-		obj = meta and meta.__index
+function Class:type()
+	return self.__type
+end
+
+function Class:typeOf(name)
+	while self do
+		if self.__type == name then return true end
+		local meta = getmetatable(self)
+		self       = meta and meta.__index
 	end
 	return false
 end
 
-function Class.__call(Class,...)
-	local obj = setmetatable({},Class)
-	if Class.init then Class.init(obj,...)end
+function Class:extend(name,...)
+	local obj  = Class.new(self,...)
+	obj.__type = name
 	return obj
-end
-
-function Class:extend(name)
-	return setmetatable( Class(name),{
-		__call  = Class.__call,
-		__index = self,
-	})
 end
 
 local reserved = {
@@ -35,28 +36,40 @@ local reserved = {
 	__index= true,
 }
 
-function Class:mixin(source,...)
-	local recur
-	recur = function(self,source)
-		local meta  = getmetatable(source)
-		local index = meta and meta.__index
+local function mixin_recursive(self,source)
+	local meta  = getmetatable(source)
+	local index = meta and meta.__index
 
-		if index then recur(self,index) end
-	
-		for i,v in pairs(source) do
-			if not reserved[i] then self[i] = v end
-		end
+	if index then mixin_recursive(self,index) end
+
+	for i,v in pairs(source) do
+		if not reserved[i] then self[i] = v end
 	end
-	recur(self,source)
+end
+
+-- http://lua-users.org/wiki/CopyTable
+local function deep_copy(t,done)
+	if done[t] then return done[t]
+	elseif type(t) ~= 'table' then return t end
 	
+	local newt = setmetatable( {}, getmetatable(t) )
+	done[t]    = newt
+	for i,v in pairs(t) do
+		i      = deep_copy(i,done)
+		v      = deep_copy(v,done)
+		newt[i]= v
+	end
+	return newt
+end
+
+function Class:mixin(source,...)
+	mixin_recursive(self,source)
 	if source.init then source.init(self,...) end
-	
 	return self
 end
 
-setmetatable(Class,{__call = function(Class,name)
-	local subClass  = {}
-	subClass.__type = name
-	subClass.__index= subClass
-	return setmetatable(subClass,Class)
-end})
+function Class:clone()
+	return deep_copy(self,{})
+end
+
+setmetatable(Class,{__call = Class.new})
